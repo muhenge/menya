2da7e381a84b9e8e94782a43dc865505d2cc8c24
NGENZI Herve <48619051+muhenge@users.noreply.github.com>
2024-09-26 08:55:14 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { config } from 'dotenv';
+
+config()
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
